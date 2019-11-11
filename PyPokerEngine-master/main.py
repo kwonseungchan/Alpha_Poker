b9ff0d8ed41d1af8443841eb0cfedab4e4dff124
Gyuni <gyuni@soongsil.ac.kr>
2019-11-12 00:13:55 +0900

    add pypokerengine

---
+++ PyPokerEngine-master/main.py
@@ -0,0 +1,10 @@
+from pypokerengine.api.game import setup_config, start_poker
+from players.fish_player import FishPlayer
+from players.console_player import ConsolePlayer
+from players.random_player import RandomPlayer
+from players.honest_player import HonestPlayer
+
+config = setup_config(max_round=30, initial_stack=1000, small_blind_amount=20)
+config.register_player(name="h1", algorithm=HonestPlayer())
+config.register_player(name="c1", algorithm=ConsolePlayer())
+game_result = start_poker(config, verbose=1)

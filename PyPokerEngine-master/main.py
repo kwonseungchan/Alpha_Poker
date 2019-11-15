556f8a32d81c097480c5a3dd19592b2373aab604
Gyuni <gyuni@soongsil.ac.kr>
2019-11-15 21:58:31 +0900

    20191115

--- PyPokerEngine-master/main.py
+++ PyPokerEngine-master/main.py
@@ -3,8 +3,9 @@ from players.fish_player import FishPlayer
 from players.console_player import ConsolePlayer
 from players.random_player import RandomPlayer
 from players.honest_player import HonestPlayer
+from players.emulator_player import EmulatorPlayer
 
 config = setup_config(max_round=30, initial_stack=1000, small_blind_amount=20)
-config.register_player(name="h1", algorithm=HonestPlayer())
-config.register_player(name="c1", algorithm=ConsolePlayer())
+config.register_player(name="Player", algorithm=HonestPlayer())
+config.register_player(name="ConsolePlayer", algorithm=ConsolePlayer())
 game_result = start_poker(config, verbose=1)
--- PyPokerEngine-master/players/honest_player.py
+++ PyPokerEngine-master/players/honest_player.py
@@ -1,5 +1,6 @@
 from pypokerengine.players import BasePokerPlayer
 from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate
+import random
 
 NB_SIMULATION = 1000
 
@@ -13,12 +14,47 @@ class HonestPlayer(BasePokerPlayer):
                 hole_card=gen_cards(hole_card),
                 community_card=gen_cards(community_card)
                 )
+
         print("승률 : ", win_rate)
-        if win_rate >= 1.0 / self.nb_player:
-            action = valid_actions[1]  # fetch CALL action info
-        else:
-            action = valid_actions[0]  # fetch FOLD action info
-        return action['action'], action['amount']
+        i = random.random()
+        if win_rate >= 0.99: #승률 99% 이상일 때
+            #amount = max(amount["min"], amount["max"]) #올인
+            #action = valid_actions[2]['amount'] #레이즈
+            return valid_actions[2]['action'], valid_actions[2]['amount']['max']
+        elif win_rate >= 0.975: #승률 97.5% 이상일 때
+            #betAmount = ( 0.1 * amount["min"]+ 0.9 * max(amount["min"], amount["max"]) )
+            #action = valid_actions[2]['amount'] #레이즈
+            return valid_actions[2]['action'], valid_actions[2]['amount']['max']
+        elif win_rate >= 0.95: #승률 95% 이상일 때
+            #betAmount = ( 0.2 * amount["min"]+ 0.8 * max(amount["min"], amount["max"]) )
+            #action = valid_actions[2]['amount'] #레이즈
+            return valid_actions[2]['action'], valid_actions[2]['amount']['max']
+        elif win_rate >= 0.9: #승률 90% 이상일 때
+            #betAmount = ( 0.5 * amount["min"]+ 0.5 * max(amount["min"], amount["max"]) )
+            #action = valid_actions[2]['amount'] #레이즈
+            return valid_actions[2]['action'], valid_actions[2]['amount']['max']
+        elif win_rate >= 0.8: #승률 80% 이상일 때
+            #amount = ( 0.2 * amount["min"]+ 0.8 * max(amount["min"], amount["max"]) )
+            #action = valid_actions[2]['amount'] #레이즈
+            return valid_actions[2]['action'], valid_actions[2]['amount']['min']
+        elif win_rate >= 0.7: #승률 70% 이상일 때
+            #action = valid_actions[1] #콜
+            return valid_actions[1]['action'], valid_actions[1]['amount']
+        elif win_rate >= 0.6: #승률 60% 이상일 때
+            #action = valid_actions[1] #콜
+            return valid_actions[1]['action'], valid_actions[1]['amount']
+        elif win_rate >= 0.5: #승률 50% 이상일 때
+            #action = valid_actions[1] #콜
+            return valid_actions[1]['action'], valid_actions[1]['amount']
+        else: 
+            return valid_actions[0]['action'], valid_actions[0]['amount']
+            #if i >= 0.90: #10% 확률로 레이즈 블러핑
+                #action = valid_actions[2] #블러핑(레이즈)
+            #elif i >= 0.80: #20% 확률로 콜 블러핑
+                #action = valid_actions[1] #블러핑(콜)
+            #else: 
+                #action = valid_actions[0]  #폴드
+        #return action['action'], action['amount']
 
     def receive_game_start_message(self, game_info):
         self.nb_player = game_info['player_num']
